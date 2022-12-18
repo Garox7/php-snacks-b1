be4a4958a97aa2b1781485e4eb798ba53710defe
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-19 00:55:39 +0100

    snack 2 completed

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,3 +1,19 @@
+<?php
+    $firstname = $_GET['firstname'] ?? '';
+    $email = $_GET['email'] ?? '';
+    $age = $_GET['age'] ?? '';
+
+    $position_at = strpos($email, '@');
+    $position_dot = strpos($email, '.', $position_at);
+
+    if (strlen($firstname) > 3 && is_numeric($age) && $position_at !== false && $position_dot !== false) {
+        $message = 'Accesso riuscito';
+    } else {
+        $message = 'Accesso negato';
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +21,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="form-container">
+        <h1>Sign In</h1>
+        <form action="" method="get">
+            <input type="text" name="firstname" placeholder="inserisci il tuo nome">
+
+            <input type="text" name="email" placeholder="inserisci la tua E-mail">
+
+            <input type="text" name="age" placeholder="inserisci la tua età">
+
+            <button>Invia</button>
+        </form>
+        <h3><?php echo $message ?></h3>
+    </div>
 </body>
 </html>
